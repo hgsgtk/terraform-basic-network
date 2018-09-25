9b7a639cb83afaec4364421a3ed5eb3cdab1b71c
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-09-25 20:03:13 +0900

    modify security group to add 80 port ingress accepted

--- main.tf
+++ main.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "wsg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress{
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags{
     Name = "web-sg"
   }
